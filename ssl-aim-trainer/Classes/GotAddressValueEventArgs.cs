c0eacead57f43094c98960eaef45560a04b532de
speyck <54800864+speyck@users.noreply.github.com>
2021-08-29 19:01:51 +0200

    Create GotAddressValueEventArgs.cs

---
+++ ssl-aim-trainer/Classes/GotAddressValueEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ssl_aim_trainer.Classes
+{
+    class GotAddressValueEventArgs : EventArgs
+    {
+        public readonly object Value;
+
+        public GotAddressValueEventArgs(object value)
+        {
+            Value = value;
+        }
+    }
+}
