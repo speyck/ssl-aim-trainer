ea212d4aa449606be6fd3e894ded2c78a307437f
speyck <54800864+speyck@users.noreply.github.com>
2021-08-29 19:05:31 +0200

    Create PointerAddresses.cs

---
+++ ssl-aim-trainer/Globals/PointerAddresses.cs
@@ -0,0 +1,11 @@
+﻿namespace ssl_aim_trainer.Globals
+{
+    internal class PointerAddresses
+    {
+        internal const string ProcessID = "ShellShockLive";
+
+        internal const string YPosPlayer = "mono.dll+001F5684,98,6cc";
+
+        internal const string YPosPointer = "UnityPlayer.dll+00FE2974,70";
+    }
+}
