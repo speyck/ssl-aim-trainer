0b28644b9fd8ca99289b01fa059e860fbd7cae23
speyck <54800864+speyck@users.noreply.github.com>
2021-08-30 15:15:09 +0200

    Use MemoryReadType, added comments and updated ReadTypes list

--- ssl-aim-trainer/Classes/PointerAddressReader.cs
+++ ssl-aim-trainer/Classes/PointerAddressReader.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading;
@@ -7,7 +6,7 @@ using Memory;
 
 namespace ssl_aim_trainer.Classes
 {
-    class PointerAddressReader
+    public class PointerAddressReader
     {
         public delegate void GotAddressValueHandler(object sender, GotAddressValueEventArgs e);
         public event GotAddressValueHandler GotAddressValue;
@@ -18,15 +17,25 @@ namespace ssl_aim_trainer.Classes
         public readonly string Address;
         public readonly string Type;
         public readonly int Interval;
+        public readonly MemoryReadType ReadType;
 
-        public PointerAddressReader(string address, string type, int interval, Mem m = null)
+        /// <summary>
+        /// Creates a new PointerAddressReader object
+        /// </summary>
+        /// <param name="address">What address should be read</param>
+        /// <param name="type">What type should be read</param>
+        /// <param name="interval">What interval the reader should have in ms</param>
+        /// <param name="m">Mem object with an open process</param>
+        public PointerAddressReader(string address, string type, int interval, Mem m)
         {
-            if (!SupportedReadTypes.Any(t => t.Key == type))
+            ReadType = ReadTypes.FirstOrDefault(t => t.ReadType == type);
+
+            if (ReadType == default(MemoryReadType))
             {
-                throw new AddressTypeNotSupportedException();
+                throw new MemoryTypeNotSupportedException(type);
             }
 
-            this.m = m ?? new();
+            this.m = m;
 
             Address = address;
             Type = type;
@@ -35,17 +44,23 @@ namespace ssl_aim_trainer.Classes
             Worker.DoWork += Worker_DoWork;
         }
 
+        /// <summary>
+        /// Starts the BackgroundWorker
+        /// </summary>
         public void RunWorker() => Worker.RunWorkerAsync();
 
+        /// <summary>
+        /// Stops the BackgroundWorker
+        /// </summary>
         public void StopWorker() => Worker.CancelAsync();
 
         private void Worker_DoWork(object sender, DoWorkEventArgs e)
         {
             while (true)
             {
-                object value = ReadMemory(Type, Address);
+                object value = ReadMemory(m, Type, Address);
 
-                GotAddressValue?.Invoke(this, new(value));
+                GotAddressValue?.Invoke(this, new(value, ReadType));
 
                 Thread.Sleep(Interval);
             }
@@ -54,43 +69,41 @@ namespace ssl_aim_trainer.Classes
         /// <summary>
         /// Reads the value of the provided address and returns it. Attention: Not all types are compatible. If the type is not compatible a AddressTypeNotSupportedException is thrown
         /// </summary>
-        /// <param name="type">The typ that should be read</param>
+        /// <param name="m">Mem object with open process to read memory</param>
+        /// <param name="type">What type should be read</param>
         /// <param name="address">What address should be read</param>
-        /// <returns></returns>
-        public static object ReadMemory(string type, string address, Mem m = null)
+        /// <returns>The read value from the address</returns>
+        public static object ReadMemory(Mem m, string type, string address, string file = "")
         {
-            m ??= new();
+            MemoryReadType readType = ReadTypes.FirstOrDefault(t => t.ReadType == type);
 
-            return type switch
-            {
-                "2byte" => m.Read2Byte(address),
-                "bits" => m.ReadBits(address),
-                "byte" => m.ReadByte(address),
-                "double" => m.ReadDouble(address),
-                "float" => m.ReadFloat(address),
-                "long" => m.ReadLong(address),
-                "int" => m.ReadInt(address),
-                "uint" => m.ReadUInt(address),
-                "string" => m.ReadString(address),
-                _ => throw new AddressTypeNotSupportedException(),
-            };
+            return readType == default(MemoryReadType) ? throw new MemoryTypeNotSupportedException(type) : readType.ReadMemory(m, address, file);
         }
 
         /// <summary>
-        /// Contains all readable types from a Mem object. The Key (all lowercase) is what value is read and the Value is what is being returned.
+        /// Same as ReadMemory method but will return the value in the given type T. Use this if you know what type is returned, otherwise use the non-generic method.
         /// </summary>
-        public static Dictionary<string, Type> SupportedReadTypes = new()
+        /// <typeparam name="T">What value the returned type is (must be correct, otherwise a exception is thrown!)</typeparam>
+        /// <param name="m">Mem object with open process to read memory</param>
+        /// <param name="type">What type should be read</param>
+        /// <param name="address">What address should be read</param>
+        /// <returns>The read value from the address as a T object</returns>
+        public static T ReadMemory<T>(Mem m, string type, string address, string file = "") => (T)ReadMemory(m, type, address, file);
+
+        /// <summary>
+        /// Contains all normal readable types from a Mem object. The readType is what value is read and the returnType is what is being returned. You need to convert the result of ReadMemory to the right type.
+        /// </summary>
+        public static readonly IEnumerable<MemoryReadType> ReadTypes = new List<MemoryReadType>()
         {
-            ["2byte"] = typeof(int),
-            ["bits"] = typeof(bool[]),
-            ["byte"] = typeof(int),
-            ["double"] = typeof(double),
-            ["float"] = typeof(float),
-            ["int"] = typeof(int),
-            ["long"] = typeof(long),
-            ["int"] = typeof(int),
-            ["uint"] = typeof(uint),
-            ["string"] = typeof(string)
+            new ("2byte",   typeof(int),    (m, code, file) => m.Read2Byte(code, file)),
+            new ("bits",    typeof(bool[]), (m, code, file) => m.ReadBits(code, file)),
+            new ("byte",    typeof(int),    (m, code, file) => m.ReadByte(code, file)),
+            new ("double",  typeof(double), (m, code, file) => m.ReadDouble(code, file)),
+            new ("float",   typeof(float),  (m, code, file) => m.ReadFloat(code, file)),
+            new ("int",     typeof(int),    (m, code, file) => m.ReadInt(code, file)),
+            new ("long",    typeof(long),   (m, code, file) => m.ReadLong(code, file)),
+            new ("uint",    typeof(uint),   (m, code, file) => m.ReadUInt(code, file)),
+            new ("string",  typeof(string), (m, code, file) => m.ReadString(code, file))
         };
     }
 }
