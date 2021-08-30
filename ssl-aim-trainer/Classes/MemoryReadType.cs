35c53f8c4ea9d4ef6d9411cbb8186ac6ff38a64c
speyck <54800864+speyck@users.noreply.github.com>
2021-08-30 15:10:20 +0200

    Create MemoryReadType.cs

---
+++ ssl-aim-trainer/Classes/MemoryReadType.cs
@@ -0,0 +1,23 @@
+﻿using Memory;
+using System;
+
+namespace ssl_aim_trainer.Classes
+{
+    public class MemoryReadType
+    {
+        public delegate object ReadMethodDelegate(Mem m, string code, string file = "");
+
+        public readonly string ReadType;
+        public readonly Type ReturnType;
+        public readonly ReadMethodDelegate ReadMethod;
+
+        public MemoryReadType(string readType, Type returnType, ReadMethodDelegate readMethod)
+        {
+            ReadType = readType;
+            ReturnType = returnType;
+            ReadMethod = readMethod;
+        }
+
+        public object ReadMemory(Mem m, string address, string file = "") => ReadMethod(m, address, file);
+    }
+}
