22ee608441fd47e39ca2c0f946ce91692f9904d5
speyck <54800864+speyck@users.noreply.github.com>
2021-08-30 15:10:49 +0200

    Create MemoryTypeNotSupportedException.cs

---
+++ ssl-aim-trainer/Classes/MemoryTypeNotSupportedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ssl_aim_trainer.Classes
+{
+    public class MemoryTypeNotSupportedException : Exception
+    {
+        public readonly string Type;
+
+        public MemoryTypeNotSupportedException(string type)
+        {
+            Type = type;
+        }
+    }
+}
