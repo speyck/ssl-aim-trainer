a122194af39eb783acfe24aace908a7a2e668004
speyck <54800864+speyck@users.noreply.github.com>
2021-08-30 15:09:47 +0200

    Create ProcessFoundEventArgs.cs

---
+++ ssl-aim-trainer/Classes/ProcessFoundEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ssl_aim_trainer.Classes
+{
+    public class ProcessFoundEventArgs : EventArgs
+    {
+        public readonly string ProcessID;
+        public readonly DateTime FoundTime;
+
+        public ProcessFoundEventArgs(string process, DateTime foundTime)
+        {
+            ProcessID = process;
+            FoundTime = foundTime;
+        }
+    }
+}
