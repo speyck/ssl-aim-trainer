c080b9381f4ebdc712ab2c1d6a222556e87f2d15
baltermia <54800864+baltermia@users.noreply.github.com>
2024-06-09 20:26:43 +0200

    Check for Process

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,43 @@
 // std
 #include <iostream>
 
+// qt
+#include <QString>
+#include <QDebug>
+#include <QProcess>
+
+// defines
+constexpr const char* SSL_PROCESS_ID = "ShellShockLive";
+
+bool is_process_running(const QString& name) 
+{
+    QProcess process;
+
+    process.start("ps -A -o comm=");
+    process.waitForFinished();
+
+    QString output(process.readAllStandardOutput());
+    QStringList list = output.split('\n');
+
+    bool isRunning = list.filter(name).size() > 0;
+
+    if (isRunning)
+        qInfo() << "Process" << name << "is running.";
+    else
+        qInfo() << "Processs" << name << "is not running.";
+
+    return isRunning;
+}
+
 int main() 
 {
-    std::cout << "Hello, World!" << std::endl;
+    qInfo() << "Started SSL Aim Trainer";
+
+    if (!is_process_running(SSL_PROCESS_ID))
+        return 0;
+
+    
+
 
     return 0;
 }
