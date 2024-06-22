383c9d6473a73a5294545452b7ccbcfa17caf5de
baltermia <54800864+baltermia@users.noreply.github.com>
2024-06-22 23:45:15 +0200

    Fix to work with msvc

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 // qt
 #include <QApplication>
 #include <QWidget>
+#include <QDebug>
+
+// std
+#include <iostream>
 
 // lib
 #include "forms/MainWindow.h"
@@ -8,12 +12,14 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    
-    QWidget window;
-    window.resize(250, 150);
-    window.setWindowTitle("Simple Qt Application");
-    window.show();
 
+	// TODO : find out where tf this output goes
+	std::cout << "Hello World!" << std::endl;
+	qDebug() << "Hello World!";
+
+	MainWindow window;
+	window.show();
+    
     return app.exec();
 }
 
