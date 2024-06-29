1ce68b42ca1270fc343fb3d69e1ce90250a85a18
baltermia <54800864+baltermia@users.noreply.github.com>
2024-06-29 13:23:20 +0200

    Draw button

--- include/forms/MainWindow.h
+++ include/forms/MainWindow.h
@@ -3,6 +3,7 @@
 // qt
 #include <QMainWindow>
 #include <QLabel>
+#include <QPushButton>
 
 // lib
 #include "process/IProcess.h"
@@ -11,7 +12,16 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-	IProcess* process;
+	std::unique_ptr<IProcess> process;
+	QLabel* lblStatus = nullptr;
+	QPushButton* btnRefresh = nullptr;
+	QLabel* lblPlayerPosY = nullptr;
+	QLabel* lblPlayerPosX = nullptr;
+
+	void redraw();
+
+private slots:
+	void btnRefresh_Clicked();
 
 public:
     MainWindow();
--- src/forms/MainWindow.cpp
+++ src/forms/MainWindow.cpp
@@ -12,20 +12,44 @@ const char* processName = "ShellShockLive.exe";
 
 MainWindow::MainWindow() 
 {
-	process = CreateProcess(processName);
+	process = std::unique_ptr<IProcess>(CreateProcess(processName));
 
-	QString status = process->is_running() ? "Running" : "Not Running";
-	QString text = QString("%1-Status: %2").arg(processName, status);
-	QLabel* statusLabel = new QLabel(text, this);
-	statusLabel->setGeometry(10, 10, 200, 30);
+	redraw();
 
     // Set the title and size of the main window
     setWindowTitle("Main Window");
     resize(400, 300);
 }
 
-MainWindow::~MainWindow()
+void MainWindow::redraw() 
 {
-	delete process;
+	QString status = process->is_running() ? "Running" : "Not Running";
+	QString text = QString("%1-Status: %2").arg(processName, status);
+	lblStatus = new QLabel(text, this);
+	lblStatus->setGeometry(10, 10, 250, 30);
+
+	btnRefresh = new QPushButton("Refresh", this);
+	btnRefresh->setGeometry(10, 50, 100, 30);
+	connect(btnRefresh, &QPushButton::clicked, this, &MainWindow::btnRefresh_Clicked);
+
+	lblPlayerPosY = new QLabel("Player Pos Y: ", this);
+	lblPlayerPosX = new QLabel("Player Pos X: ", this);
+
+	lblPlayerPosY->setGeometry(10, 90, 250, 30);
+	lblPlayerPosX->setGeometry(10, 130, 250, 30);
+}
+
+void MainWindow::btnRefresh_Clicked()
+{
+	lblPlayerPosX->setText(QString("Player Pos X: %1").arg("-"));
+	lblPlayerPosY->setText(QString("Player Pos Y: %1").arg("-"));
+}
+
+MainWindow::~MainWindow()
+{ 
+	delete lblStatus;
+	delete btnRefresh;
+	delete lblPlayerPosY;
+	delete lblPlayerPosX;
 }
 
